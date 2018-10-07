7aa5558f336c163e9f259f0f11f43d433d8872f9
idasand <ida.sandsbraaten@gmail.com>
2018-10-07 11:11:47 +0200

    function name add

--- implementations.py
+++ implementations.py
@@ -1,1 +1,22 @@
-//all implementation goes here 
+//all implementation goes here
+
+import numpy as np
+
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+    return
+
+
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+    return
+
+def least_squares(y, tx):
+    return
+
+def ridge_regression(y, tx, initial_w, max_iters, gamma):
+    return
+
+def logistic_regression(y, tx, initial_w, max_iters, gamma):
+    return
+
+def reg_logistic_regression(y, tx, initial_w, max_iters, gamma):
+    return
